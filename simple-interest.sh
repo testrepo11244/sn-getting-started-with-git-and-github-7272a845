c417bd29618a29d8a5d98eb7bbda3eac6d6aa855
testrepo11244 <vivocaloid@gmail.com>
2026-07-20 00:17:40 +0700

    add simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 # Simple Interest Calculator
-
 echo "Enter the principal amount:"
-read principal
-
+read p
 echo "Enter the rate of interest (in %):"
-read rate
-
+read r
 echo "Enter the time period (in years):"
-read time
-
-simple_interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
-
-echo "The simple interest is: $simple_interest"
+read t
+s=`echo $p \* $r \* $t / 100 | bc`
+echo "The simple interest is: $s"
